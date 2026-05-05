8105c6f060362695d975a3d8c048080aec9ecdd9
Brett Pearson <131393660+BPear1225@users.noreply.github.com>
2026-05-04 22:53:36 -0500

    Basic DeliveryPlan implementation

--- enchanted-courier-guild/src/main/java/edu/oop/guild/model/DeliveryPlan.java
+++ enchanted-courier-guild/src/main/java/edu/oop/guild/model/DeliveryPlan.java
@@ -1,5 +1,51 @@
 package edu.oop.guild.model;
 
+import edu.oop.guild.creature.Creature;
+
 public class DeliveryPlan {
+
+	private DeliveryRequest request;
+	private Creature courier;
+	private String sealLabel;
+	private int price;
+
+	public DeliveryPlan(DeliveryRequest request, Creature courier, String label, int price) {
+		if (request == null) {
+			throw new NullPointerException("Must have a request");
+		}
+		if (courier == null) {
+			throw new NullPointerException("Must have someone to diliver the package");
+		}
+		if (label == null) {
+			throw new NullPointerException("Must have a lable");
+		}
+		if (price < 0) {
+			throw new IllegalArgumentException("Price can not be negative");
+		}
+		this.request = request;
+		this.courier = courier;
+		this.sealLabel = label;
+		this.price = price;
+	}
+
+	public PackageType getRequest() {
+		return request.getPackageType();
+	}
+
+	public String getCourier() {
+		return courier.name();
+	}
+
+	public String getSealedLabel() {
+		return sealLabel;
+	}
+
+	public int getPriceInCoins() {
+		return price;
+	}
+
+	public String summary() {
+		return getCourier() + " delivers " + getSealedLabel() + " for " + getPriceInCoins() + " coins";
+	}
 
 }
