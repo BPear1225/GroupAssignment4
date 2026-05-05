4d4ab71c03952c0b432b68a5a69426cf56110590
Brett Pearson <131393660+BPear1225@users.noreply.github.com>
2026-05-04 22:37:12 -0500

    Implemented DeliveryRequest basic functions

--- enchanted-courier-guild/src/main/java/edu/oop/guild/model/DeliveryRequest.java
+++ enchanted-courier-guild/src/main/java/edu/oop/guild/model/DeliveryRequest.java
@@ -1,10 +1,52 @@
 package edu.oop.guild.model;
 
 public class DeliveryRequest {
+	
+	private PackageType packageType;
+	private int weightKg;
+	private int distanceLeagues;
+	private RealmType destinationRealm;
+	private boolean fragile;
 
 	public DeliveryRequest(PackageType packageType, int weightKg, int distanceLeagues, RealmType destinationRealm,
 			boolean fragile) {
-		// TODO Auto-generated constructor stub
+		if (packageType == null) {
+			throw new NullPointerException("Must have a Package Type");
+		}
+		if (weightKg <=0) {
+			throw new IllegalArgumentException("Must have a positive weight");
+		}
+		if (distanceLeagues <=0) {
+			throw new IllegalArgumentException("Must have a positive distance");
+		}
+		if (destinationRealm == null) {
+			throw new NullPointerException("Must have a destination");
+		}
+		this.packageType = packageType;
+		this.weightKg = weightKg;
+		this.distanceLeagues = distanceLeagues;
+		this.destinationRealm = destinationRealm;
+		this.fragile = fragile;
+	}
+
+	public PackageType getPackageType() {
+		return packageType;
+	}
+
+	public int getWeightKg() {
+		return weightKg;
+	}
+
+	public int getDistanceLeagues() {
+		return distanceLeagues;
+	}
+
+	public RealmType getDestinationRealm() {
+		return destinationRealm;
+	}
+
+	public boolean isFragile() {
+		return fragile;
 	}
 
 }
